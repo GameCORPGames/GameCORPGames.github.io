48c847e351729faf29409a2827365ae30fd967ec
GameCORPGames <30369452+GameCORPGames@users.noreply.github.com>
2017-09-16 14:05:59 +0800

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,8 +1,13 @@
+var wHeight = $(window).height();
+var wWidth = $(window).width();
+var pWide = wWidth / 20;
 $(document).ready(function(){
+    $("#header-links").css("padding", "1em " + pWide + " 1em " + pWide);
     $("a").mouseenter(function(){
         $(this).fadeTo("slow", 0.5);
     });
-	$("a").mouseleave(function(){
+    $("a").mouseleave(function(){
         $(this).fadeTo("slow", 1);
     });
 });
+alert("padding", "1em " + pWide + " 1em " + pWide);
